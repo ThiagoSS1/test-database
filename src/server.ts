a6de25e84939b04173fc4f6906a53bb7884c4158
ThiagoSS1 <thiago_seta@hotmail.com>
2022-01-10 21:21:03 -0300

    alterando script dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response } from 'express';
 import "reflect-metadata";
+import "dotenv/config";
+import cors from "cors"
 import Database from './core/data/connections/Database';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 })
-
 
 new Database()
     .openConnection()
